d009fa990df581b2b3d8e326e1f84c69459e9beb
monge <alberto.monge@polito.it>
2022-04-05 14:39:15 +0200

    move Task4.3 to another repo

--- policy-translation-point/server/src/main/java/it/polito/elite/sifis/entities/db/Dbpolicy.java
+++
@@ -1,5 +0,0 @@
-package it.polito.elite.sifis.entities.db;
-
-public class Dbpolicy extends Dbrule{
-
-}
--- policy-translation-point/server/src/main/java/it/polito/elite/sifis/repositories/TriggerRepository.java
+++
@@ -1,9 +0,0 @@
-package it.polito.elite.sifis.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.polito.elite.sifis.entities.db.Dbtrigger;
-
-public interface TriggerRepository extends JpaRepository<Dbtrigger, Long>{
-
-}
